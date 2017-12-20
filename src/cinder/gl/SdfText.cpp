cac4d9babbcb32c14ce3ce78240da3fda76b9fac
Shawn Lipowski <slipowski@gmail.com>
2017-12-20 12:15:20 -0500

    uFgColor.a not gamma correct with opactiy in frag shader -- allows for smooth animation of alpha() in NodeText

--- src/cinder/gl/SdfText.cpp
+++ src/cinder/gl/SdfText.cpp
@@ -206,7 +206,7 @@ static std::string kSdfFragShader =
 	"    float afwidth = min( kNormalization * length( grad ), 0.5 );\n"
 	"    float opacity = smoothstep( 0.0 - afwidth, 0.0 + afwidth, sigDist );\n"
     "    // If enabled apply pre-multiplied alpha. Always apply gamma correction.\n"
-	"    Color.a = pow( uFgColor.a * opacity, 1.0 / uGamma );\n"
+	"    Color.a = uFgColor.a * pow( opacity, 1.0 / uGamma );\n"
 	"    Color.rgb = mix( uFgColor.rgb, uFgColor.rgb * Color.a, uPremultiply );\n"
 //	"    Color = vec4( 1, 0, 0, 1 );\n"
 	"}\n";
